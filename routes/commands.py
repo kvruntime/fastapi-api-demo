1fa27ee9c81d45829709b59231c24cf4f025520c
kvruntime <kvruntime@gmail.com>
2023-03-08 00:13:54 +0000

    fix() manage routes response status code

--- routes/commands.py
+++ routes/commands.py
@@ -1,9 +1,10 @@
 from uuid import UUID
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, status
 from dbdomain.models import Command
 from dtos.commanddtos import CreateCommandDto, UpdateCommandDto
 from shared.commonrepo import CommonRepo
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, Response
+from tortoise.exceptions import DoesNotExist
 
 command_repo = CommonRepo[Command](Command)
 
@@ -17,27 +18,47 @@ async def get_commands():
 
 @command_router.get("/{id}")
 async def get_command(id: UUID):
-    return await command_repo.read(id)
+    try:
+        return await command_repo.read(id)
+    except DoesNotExist:
+        return Response(
+            content=f"Command with id:{id} not found",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
 
 
 @command_router.delete("/{id}")
 async def delete_command(id: UUID):
-    await command_repo.delete(id)
-    return None
+    try:
+        await command_repo.delete(id)
+    except DoesNotExist:
+        return Response(
+            content=f"Command with id:{id} not found",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @command_router.put("/{id}")
 async def put_command(id: UUID, command: UpdateCommandDto):
     # FIXME: this is the first way (use update function from repository)
     # await command_repo.update(id, Command(**command.dict()))
-
-    _item = await command_repo.read(id)
-    await _item.update_from_dict(command.dict(exclude_unset=True))
-    await command_repo.save(_item)
-    return
+    try:
+        _item = await command_repo.read(id)
+        await _item.update_from_dict(command.dict(exclude_unset=True))
+        await command_repo.save(_item)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    except DoesNotExist:
+        return Response(
+            content=f"Command with id:{id} not found",
+            status_code=status.HTTP_404_NOT_FOUND
+        )
 
 
 @command_router.post("/")
 async def post_command(request: Request, command: CreateCommandDto):
     _item = await command_repo.insert(Command(**command.dict()))
-    return RedirectResponse(request.url_for("get_command", **dict(id=_item.id)), status_code=303)
+    return RedirectResponse(
+        request.url_for("get_command", **dict(id=_item.id)),
+        status_code=status.HTTP_201_CREATED
+    )
